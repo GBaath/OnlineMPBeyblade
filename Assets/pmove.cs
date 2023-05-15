f4077a3e57fd09f50f7a3b4ee04e6f7364660ecb
GVBååth <gustav.baath02@gmail.com>
2023-05-15 11:06:35 +0200

    Initial commit

---
+++ Assets/pmove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Alteruna;
+
+public class pmove : MonoBehaviour
+{
+    const string horizontal = "Horizontal";
+    const string vertical = "Vertical";
+
+
+    private Alteruna.Avatar avatar;
+
+    void Update()
+    {
+        if (!avatar.IsMe)
+            return;
+
+
+        if (Input.GetButtonDown(horizontal))
+        {
+            Vector2 move = Input.GetAxisRaw(horizontal) * Vector2.right;
+            transform.position += (Vector3)move;
+        }
+        if (Input.GetButtonDown(vertical))
+        {
+            Vector2 move = Input.GetAxisRaw(vertical) * Vector2.up;
+            transform.position += (Vector3)move;
+        }
+    }
+}
