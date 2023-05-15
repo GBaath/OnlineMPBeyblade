890479e771a80e0992096875faaba861d1c05885
GVBååth <gustav.baath02@gmail.com>
2023-05-15 11:38:44 +0200

    change version

--- Assets/pmove.cs
+++ Assets/pmove.cs
@@ -6,15 +6,21 @@ using Unity.VisualScripting;
 
 public class pmove : MonoBehaviour
 {
-    const string horizontal = "Horizontal";
-    const string vertical = "Vertical";
+    const string HORIZONTAL = "Horizontal";
+    const string VERTICAL = "Vertical";
 
 
     private Alteruna.Avatar avatar;
 
-
-    private void Start()
+    public float speed = 1;
+    private Vector2 move;
+    private Vector2 automove;
+    private float moveX, moveY;
+    private Rigidbody2D rb;
+    [SerializeField] ParticleSystem runParticles;
+    void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         avatar = GetComponent<Alteruna.Avatar>();
     }
 
@@ -24,15 +30,54 @@ public class pmove : MonoBehaviour
             return;
 
 
-        if (Input.GetButtonDown(horizontal))
+        moveY = Input.GetAxis(VERTICAL);
+        moveX = Input.GetAxis(HORIZONTAL);
+
+        move = moveX * Vector3.right + moveY * Vector3.up;
+
+        if (Input.GetButton(HORIZONTAL))
         {
-            Vector2 move = Input.GetAxisRaw(horizontal) * Vector2.right;
-            transform.position += (Vector3)move;
+            if (moveX < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                runParticles.transform.localEulerAngles = new Vector3(0, 180, 0);
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                runParticles.transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
         }
-        if (Input.GetButtonDown(vertical))
+        if (move != Vector2.zero)
         {
-            Vector2 move = Input.GetAxisRaw(vertical) * Vector2.up;
-            transform.position += (Vector3)move;
+            //runParticles.gameObject.SetActive(true);
+            runParticles.enableEmission = true;
         }
+        else
+        {
+            runParticles.enableEmission = false;
+            //runParticles.gameObject.SetActive(false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(move * speed + automove);
+    }
+
+    private IEnumerator LerpAutomove(Vector2 startValue, Vector2 endVal, float duration)
+    {
+        float time = 0;
+        while (time < duration)
+        {
+            automove = Vector2.Lerp(startValue, endVal, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        automove = endVal;
+    }
+    IEnumerator VarChange(System.Action<bool> boolVar, float cooldown, bool endValue)
+    {
+        yield return new WaitForSeconds(cooldown);
+        boolVar(endValue);
     }
 }
